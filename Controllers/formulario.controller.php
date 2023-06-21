2c264f63bd471de7fd806eac606a9c1091079841
Edgar Apaza <edgarapazac@gmail.com>
2023-06-21 15:59:43 -0500

    cambios para eliminar

--- Controllers/formulario.controller.php
+++ Controllers/formulario.controller.php
@@ -1,39 +1,25 @@
-<?php
-
+}<?php
 require '../Models/formulario.models.php';
 
  //instanciar a la clase para Registrar
 $registro = new Producto();
 
-echo $fecharecepcion = $_POST['fecharecepcion'];
-echo $numsoli		= $_POST['numsoli'];
-echo $nombre 		= $_POST['txtnombre'];
-echo $notario 		= $_POST['notario'];
-echo $tipodoc		= $_POST['tipodoc'];
-echo $fechaatendida 	= $_POST['fechaatendida'];
-echo $observacion 	= $_POST['observacion'];
-echo $estado			= $_POST['estado'];
-
+$fecharecepcion = $_POST['fecharecepcion'];
+$numsoli		= $_POST['numsoli'];
+$nombre 		= $_POST['txtnombre'];
+$notario 		= $_POST['notario'];
+$tipodoc		= $_POST['tipodoc'];
+$fechaatendida 	= $_POST['fechaatendida'];
+$observacion 	= $_POST['observacion'];
+$estado			= $_POST['estado'];
 
 //llamar a la funcion Registrar
 
-$respuesta = $registro->Registrar($fecharecepcion,$numsoli,$nombre,$notario,$tipodoc,$fechaatendida,$observacion,$estado,'');
+$respuesta = $registro->Registrar($fecharecepcion,$numsoli,$nombre,$notario,$tipodoc,$fechaatendida,$observacion,$estado,null);
 
-if($respuesta == true)
-	header("Location: ../View/principal.php?msg=Guardado Correctamente");
-else
-    header("Location: ../View/error.php");
-
-
-
-
-//Instancias a la clase para Eliminacion
-//$eliminacion = new Eliminacion();
-
-//Llamar a la funcion Eliminar
-
-//$respuesta = $eliminacion->Eliminar($id,$producto,$marca,$costo,$precio,$cantidad,$imagen);
-//if($respuesta == true)
-//	header("Location: ../View/principal.php");
-//else
-	//header("Location: ../View/error.php");
+if($respuesta)
+{
+   header("Location: ../View/principal.php?msg=Guardado Correctamente");
+}else{
+   header("Location: ../View/error.php");
+}
